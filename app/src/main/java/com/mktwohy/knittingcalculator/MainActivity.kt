0e2372b5cc3b35f0bd5a1a789ed87fd6367182e2
mktwohy <69822801+mktwohy@users.noreply.github.com>
2023-02-12 23:21:24 -0600

    ensure focus is cleared when clicking outside textfields

--- app/src/main/java/com/mktwohy/knittingcalculator/MainActivity.kt
+++ app/src/main/java/com/mktwohy/knittingcalculator/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActionScope
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,6 +26,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mktwohy.knittingcalculator.extensions.noRippleClickable
 import com.mktwohy.knittingcalculator.ui.theme.KnittingCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,8 +37,10 @@ class MainActivity : ComponentActivity() {
             val focusManager = LocalFocusManager.current
             KnittingCalculatorTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .noRippleClickable { focusManager.clearFocus() }
                 ) {
                     Column {
                         FormulaCard(
---
+++ app/src/main/java/com/mktwohy/knittingcalculator/extensions/ModifierExtensions.kt
@@ -0,0 +1,26 @@
+package com.mktwohy.knittingcalculator.extensions
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.semantics.Role
+
+fun Modifier.noRippleClickable(
+    enabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    onClick: () -> Unit
+): Modifier =
+    composed {
+        this then Modifier
+            .clickable(
+                enabled = enabled,
+                onClickLabel = onClickLabel,
+                role = role,
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = onClick
+            )
+    }
