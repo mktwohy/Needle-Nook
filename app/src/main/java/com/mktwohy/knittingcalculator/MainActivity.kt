c98d6634621d00113164ae9b92a8e748a60efc65
mktwohy <69822801+mktwohy@users.noreply.github.com>
2023-02-12 22:35:08 -0600

    add basic UI for FormulaCard

--- app/src/main/java/com/mktwohy/knittingcalculator/MainActivity.kt
+++ app/src/main/java/com/mktwohy/knittingcalculator/MainActivity.kt
@@ -3,18 +3,27 @@ package com.mktwohy.knittingcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mktwohy.knittingcalculator.ui.theme.KnittingCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val viewModel: MainViewModel by viewModels()
         setContent {
             KnittingCalculatorTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +31,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        FormulaCard(
+                            inputs = listOf(viewModel.density, viewModel.length),
+                            output = viewModel.stitchCount
+                        )
+                    }
                 }
             }
         }
@@ -30,14 +44,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FormulaCard(
+    inputs: List<Input<String>>,
+    output: String,
+) {
+    Box(
+        Modifier
+            .background(
+                color = Color.DarkGray,
+                shape = RoundedCornerShape(8.dp)
+            )
+    ) {
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+                for (input in inputs) {
+                    OutlinedTextField(
+                        value = input.value,
+                        onValueChange = input.onValueChange,
+                        trailingIcon = {
+                            Text(
+                                text = input.unit,
+                                modifier = Modifier.padding(end = 4.dp)
+                            )
+                        },
+                        label = { Text(input.name) },
+                        placeholder = { Text(input.name) },
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = MaterialTheme.colors.background
+                        ),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Decimal
+                        )
+                    )
+                }
+            }
+            Text(output)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.PIXEL_3A)
 @Composable
 fun DefaultPreview() {
     KnittingCalculatorTheme {
-        Greeting("Android")
+        FormulaCard(
+            inputs = listOf(
+                Input(initValue = "0.0", name = "Input 1", unit = "Unit"),
+                Input(initValue = "", name = "Input 2", unit = "Unit"),
+            ),
+            output = "3.0"
+        )
     }
 }
--- app/src/main/java/com/mktwohy/knittingcalculator/MainViewModel.kt
+++ app/src/main/java/com/mktwohy/knittingcalculator/MainViewModel.kt
@@ -2,18 +2,14 @@ package com.mktwohy.knittingcalculator
 
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
-    object StitchesPerInch {
-        var density by mutableStateOf("")
-        var length by mutableStateOf("")
-        val stitchCount by derivedStateOf {
-            val density = this.density.toFloatOrNull() ?: return@derivedStateOf null
-            val length = this.length.toFloatOrNull() ?: return@derivedStateOf null
-            getStitchCount(density, length).toString()
-        }
+    var density = Input("", name = "Density", unit = "Stitch/Inch")
+    var length = Input("", name = "Length", unit = "Inch")
+    val stitchCount by derivedStateOf {
+        val density = this.density.value.toFloatOrNull() ?: return@derivedStateOf ""
+        val length = this.length.value.toFloatOrNull() ?: return@derivedStateOf ""
+        getStitchCount(density, length).toString()
     }
 }
