90055461dda418d418cc4ac685aa44f99f650d3c
mktwohy <mktwohy@gmail.com>
2023-09-23 22:31:34 -0500

    add MarkdownStyle sealed class

--- app/src/main/java/com/mktwohy/needlenook/ui/composables/projectscreen/Markdown.kt
+++ app/src/main/java/com/mktwohy/needlenook/ui/composables/projectscreen/Markdown.kt
@@ -20,6 +20,17 @@ import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
 import org.intellij.markdown.html.HtmlGenerator
 import org.intellij.markdown.parser.MarkdownParser
 
+private fun ASTNode.traverse(onVisitNode: (ASTNode) -> Unit) {
+    accept(
+        object : Visitor {
+            override fun visitNode(node: ASTNode) {
+                onVisitNode(node)
+                node.acceptChildren(this)
+            }
+        }
+    )
+}
+
 @JvmInline
 value class Markdown(val value: String) : CharSequence by value {
     fun parseAST(flavor: MarkdownFlavourDescriptor = Defaults.mdFlavor): ASTNode =
@@ -28,27 +39,16 @@ value class Markdown(val value: String) : CharSequence by value {
     fun parseHtml(flavor: MarkdownFlavourDescriptor = Defaults.mdFlavor): String =
         HtmlGenerator(value, parseAST(flavor), flavor).generateHtml()
 
-    fun toAnnotatedString(
-        flavor: MarkdownFlavourDescriptor = Defaults.mdFlavor,
-        elementTypeToSpanStyle: Map<IElementType, SpanStyle> = Defaults.mdElementTypeToSpanStyle,
-        elementTypeToParagraphStyle: Map<IElementType, ParagraphStyle> = Defaults.mdElementTypeToParagraphStyle
-    ): AnnotatedString {
+    fun toAnnotatedString(flavor: MarkdownFlavourDescriptor = Defaults.mdFlavor): AnnotatedString {
         val md = this
 
         return buildAnnotatedString {
             append(md)
 
             md.parseAST(flavor).traverse { node ->
-                elementTypeToSpanStyle[node.type]?.let { style ->
+                MarkdownStyle.fromIElementType(node.type)?.spanStyle?.let {
                     addStyle(
-                        style = style,
-                        start = node.startOffset,
-                        end = node.endOffset
-                    )
-                }
-                elementTypeToParagraphStyle[node.type]?.let { style ->
-                    addStyle(
-                        style = style,
+                        style = it,
                         start = node.startOffset,
                         end = node.endOffset
                     )
@@ -60,53 +60,48 @@ value class Markdown(val value: String) : CharSequence by value {
     companion object {
         object Defaults {
             val mdFlavor = CommonMarkFlavourDescriptor()
-            val mdElementTypeToSpanStyle = mapOf(
-                // ATX: header
-                MarkdownElementTypes.ATX_1 to SpanStyle(
-                    fontSize = 36.sp,
-                    fontWeight = FontWeight.Medium
-                ),
-                MarkdownElementTypes.ATX_2 to SpanStyle(
-                    fontSize = 32.sp,
-                    fontWeight = FontWeight.Medium
-                ),
-                MarkdownElementTypes.ATX_3 to SpanStyle(
-                    fontSize = 28.sp,
-                    fontWeight = FontWeight.Medium
-                ),
-                MarkdownElementTypes.ATX_4 to SpanStyle(
-                    fontSize = 25.sp,
-                    fontWeight = FontWeight.Medium
-                ),
-                MarkdownElementTypes.ATX_5 to SpanStyle(
-                    fontSize = 22.sp,
-                    fontWeight = FontWeight.Medium
-                ),
-                MarkdownElementTypes.ATX_6 to SpanStyle(
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Medium
-                ),
-
-                MarkdownElementTypes.EMPH to SpanStyle(fontStyle = FontStyle.Italic),
-                MarkdownElementTypes.STRONG to SpanStyle(fontWeight = FontWeight.Bold),
-                MarkdownElementTypes.CODE_SPAN to SpanStyle(fontFamily = FontFamily.Monospace),
-                MarkdownTokenTypes.LIST_BULLET to SpanStyle(fontWeight = FontWeight.Light),
-                MarkdownTokenTypes.LIST_NUMBER to SpanStyle(fontWeight = FontWeight.Light),
-            )
-            val mdElementTypeToParagraphStyle = mapOf<IElementType, ParagraphStyle>(
-
-            )
         }
     }
 }
 
-private fun ASTNode.traverse(onVisitNode: (ASTNode) -> Unit) {
-    accept(
-        object : Visitor {
-            override fun visitNode(node: ASTNode) {
-                onVisitNode(node)
-                node.acceptChildren(this)
+sealed class MarkdownStyle {
+    abstract val spanStyle: SpanStyle
+
+    sealed class HeaderStyle(override val spanStyle: SpanStyle) : MarkdownStyle()
+    data object H1 : HeaderStyle(SpanStyle(fontSize = 36.sp, fontWeight = FontWeight.Medium))
+    data object H2 : HeaderStyle(SpanStyle(fontSize = 32.sp, fontWeight = FontWeight.Medium))
+    data object H3 : HeaderStyle(SpanStyle(fontSize = 28.sp, fontWeight = FontWeight.Medium))
+    data object H4 : HeaderStyle(SpanStyle(fontSize = 25.sp, fontWeight = FontWeight.Medium))
+    data object H5 : HeaderStyle(SpanStyle(fontSize = 22.sp, fontWeight = FontWeight.Medium))
+    data object H6 : HeaderStyle(SpanStyle(fontSize = 20.sp, fontWeight = FontWeight.Medium))
+
+    sealed class InlineStyle(override val spanStyle: SpanStyle) : MarkdownStyle()
+    data object Italic : InlineStyle(SpanStyle(fontStyle = FontStyle.Italic))
+    data object Bold : InlineStyle(SpanStyle(fontWeight = FontWeight.Bold))
+    data object Code : InlineStyle(SpanStyle(fontFamily = FontFamily.Monospace))
+
+    sealed class ListStyle(override val spanStyle: SpanStyle) : MarkdownStyle()
+    data object ListItem : ListStyle(SpanStyle())
+    data object ListBullet : ListStyle(SpanStyle(fontWeight = FontWeight.Light))
+    data object ListNumber : ListStyle(SpanStyle(fontWeight = FontWeight.Light))
+
+    companion object {
+        fun fromIElementType(elementType: IElementType): MarkdownStyle? {
+            return when (elementType) {
+                MarkdownElementTypes.ATX_1 -> H1
+                MarkdownElementTypes.ATX_2 -> H2
+                MarkdownElementTypes.ATX_3 -> H3
+                MarkdownElementTypes.ATX_4 -> H4
+                MarkdownElementTypes.ATX_5 -> H5
+                MarkdownElementTypes.ATX_6 -> H6
+                MarkdownElementTypes.EMPH -> Italic
+                MarkdownElementTypes.STRONG -> Bold
+                MarkdownElementTypes.CODE_SPAN -> Code
+                MarkdownElementTypes.LIST_ITEM -> ListItem
+                MarkdownTokenTypes.LIST_BULLET -> ListBullet
+                MarkdownTokenTypes.LIST_NUMBER -> ListNumber
+                else -> null
             }
         }
-    )
+    }
 }
